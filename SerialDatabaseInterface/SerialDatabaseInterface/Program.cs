3c2e896c6ead06f0246cccfd5a645ec52297b617
E2658 <lucafymail@gmail.com>
2026-09-21 10:02:47 +0200

    Added SerialDatabaseInterface Project

---
+++ SerialDatabaseInterface/SerialDatabaseInterface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SerialDatabaseInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
